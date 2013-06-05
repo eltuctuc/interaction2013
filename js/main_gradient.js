5a8931417180834fd2c0780d78e1fe3f51a412c0
Max <tubedesignz@googlemail.com>
2013-06-05 22:38:45 +0200

    enthält jetzt gradient
    Ich habe es geschafft einen radialen Verlauf zu zeichnen. Das Rechteck
    in welches der Verlauf gezeichnet wird passt sich automatisch an den
    Radius des Verlaufs an.

--- js/main_gradient.js
+++ js/main_gradient.js
@@ -36,10 +36,15 @@ function init () {
 
 function drawImg( img ) {
 
-var width = context.canvas.width;
-var height = context.canvas.height;
+var grad_size = 100; //äußere Größe des Grad und Rect bestimmen
+var x_pos = 200-grad_size; //x-position des Grad-Mittelpunktes bestimmen
+var y_pos = 300-grad_size; //y-position des Grad-Mittelpunktes bestimmen
 
- context.drawImage(img,0,0,$('#canvas').width(),$('#canvas').height());
- context.fillStyle = 'rgb(255, 0, 0)';
- context.fillRect(20, height-(height*0.2), width/30, height*0.2);
+context.drawImage(img,0,0,$('#canvas').width(),$('#canvas').height());
+var gradient = context.createRadialGradient(x_pos+grad_size,y_pos+grad_size,5,x_pos+grad_size,y_pos+grad_size,grad_size);
+	gradient.addColorStop(0, '#000000');
+	gradient.addColorStop(1, '#FFFFFF');
+	
+	context.fillStyle = gradient;
+	context.fillRect(x_pos,y_pos,grad_size*2,grad_size*2);
 }
