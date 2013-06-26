324cea2ee28ce9a4efc74ef30882402f8a5ba6ab
Enrico Reinsdorf <enrico.reinsdorf@re-design.de>
2013-06-26 14:54:22 +0200

    add App Framwork

---
+++ js/app.page.js
@@ -0,0 +1,20 @@
+App.Page = {
+	pages : [],
+	init : function () {
+		$('.page').each(function() {
+			App.Page.pages.push($(this));
+		});
+
+		this.showPage('page_3');
+	},
+
+	showPage : function (name) {
+		for (var i = 0; i < this.pages.length; i++) {
+			if($(this.pages[i]).attr('id') == name) {
+				$(this.pages[i]).fadeIn();
+				continue;
+			}
+			$(this.pages[i]).hide();
+		};
+	}
+}
--- js/app_sichtfilter.js
+++ js/app_sichtfilter.js
@@ -1,31 +1,3 @@
-var App = {
-	init : function () {
-		this.Page.init();
-	},
-};
-
-App.Page = {
-	pages : [],
-	init : function () {
-		$('.page').each(function() {
-			App.Page.pages.push($(this));
-		});
-
-		this.showPage('page_3');
-	},
-
-	showPage : function (name) {
-		for (var i = 0; i < this.pages.length; i++) {
-			if($(this.pages[i]).attr('id') == name) {
-				$(this.pages[i]).fadeIn();
-				continue;
-			}
-			$(this.pages[i]).hide();
-		};
-	}
-}
-
-
 $(document).ready(function($) {
 
 	$('.slider').slider({
